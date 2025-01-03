cc716cd5f8c7ab3eb4878fabf7dec02f7fe97869
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2025-01-03 09:38:10 +0530

    commit

--- git.py
+++ git.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(1,35):
+for i in range(1,15):
     for j in range(0,randint(1,10)):
         d = str(i) + ' days ago'
         with open('file.txt','a') as file:
